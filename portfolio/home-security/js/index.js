3a969142a85aa74ae327e0a825b924330d8346b7
Yegor Murunov <77667473+YegorMurunov@users.noreply.github.com>
2021-12-31 17:53:07 +0200

    Add new work and update resume

---
+++ portfolio/home-security/js/index.js
@@ -0,0 +1,1 @@
+const menuBtn=document.querySelector(".header__menu");headerNav=document.querySelector(".header__nav");menuBtn.addEventListener("click",()=>{menuBtn.classList.toggle("active");document.body.classList.toggle("lock");headerNav.classList.toggle("active")});
--- portfolio/pc-build/js/main.js
+++ portfolio/pc-build/js/main.js
@@ -1,16 +1,1 @@
-const closeIcon = document.querySelector('.header__menu-icon');
-const nav = document.querySelector('.header__nav');
-
-if (closeIcon) {
-	closeIcon.addEventListener('click', () => {
-		if (closeIcon.classList.contains('active')) {
-			closeIcon.classList.remove('active');
-			nav.classList.remove('active');
-			document.body.classList.remove('lock');
-		} else {
-			closeIcon.classList.add('active');
-			nav.classList.add('active');
-			document.body.classList.add('lock');
-		}
-	});
-}
+const closeIcon=document.querySelector(".header__menu-icon"),nav=document.querySelector(".header__nav");closeIcon&&closeIcon.addEventListener("click",()=>{closeIcon.classList.contains("active")?(closeIcon.classList.remove("active"),nav.classList.remove("active"),document.body.classList.remove("lock")):(closeIcon.classList.add("active"),nav.classList.add("active"),document.body.classList.add("lock"))});
