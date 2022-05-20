56a46fc74fc1d511c0d054bc0b9cbbed0701e92d
Yegor Murunov <77667473+YegorMurunov@users.noreply.github.com>
2022-05-20 17:33:17 +0300

    new work sweet-chest

---
+++ portfolio/sweet-chest/js/swiper.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var _swiperBundleEsmBrowserMin = _interopRequireDefault(require("https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var swiper = new _swiperBundleEsmBrowserMin["default"]('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  speed: 800,
+  spaceBetween: 80,
+  loop: false,
+  slidesPerView: 1.25,
+  centeredSlides: true,
+  effect: "coverflow",
+  // cards
+  coverflowEffect: {
+    rotate: 0,
+    depth: 100,
+    slideShadows: true
+  },
+  allowTouchMove: true,
+  grabCursor: true,
+  autoHeight: false,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      stretch: 1
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.05,
+      spaceBetween: 50,
+      stretch: 1
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1.25,
+      spaceBetween: 80
+    }
+  },
+  // navigation
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    disabledClass: '--disabled'
+  }
+});
